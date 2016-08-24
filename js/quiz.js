33d7cb2066eb371670139c4dff1d0aff927ff945
maitabe <martushb@gmail.com>
2016-08-24 23:38:00 +0300

    finilized the dynamic of questions

--- js/quiz.js
+++ js/quiz.js
@@ -5,19 +5,53 @@ var quizService,
 	sortQuestObj,
 	totalQ,
 	question,
-	answers;
+	answers,
+	score = 0,
+	scoreWrong = 0;
 
+// loop to get different answers
+function getAnswers() {
 
+	for (var i = 0; i < answers.length; i++) {
+
+				$('#answers').append('<label><input type="radio" name="answer" data-index= '+i+' value="' + answers[i] + '">' + answers[i] + '</label><br />');
+				console.log(i);
+				console.log(answers[i]);
+			}
+}
+
+// catch the answer selected
 function registerAnswers() {
 
 			$('input[type="radio"]').change(function() {
 
-				//get value of selected answer
+				// get value of selected answer
 				var answerSel = $('input:checked').val();
+
+				//get 'data' attribute from selected answer
+				var answerIndex = $('input:checked').data('index');
+
+				//convert string to a number
+				var correctAnswer =  parseInt(sortQuestObj[currentQ].correctAnswer);
+
+					//compare selected answer vs correct answer
+					if(answerIndex === correctAnswer) {
+						console.log('bingo');
+						score  = score + 1;
+
+					}else {
+						console.log('try again!');
+
+						scoreWrong = scoreWrong + 1;
+					}
+
 				//display "next" button to go to the next question
 				$('#nextQuestion').css('visibility', 'visible');
+				 console.log(score);
+				 console.log(scoreWrong);
 			});
 }
+
 
 $(document).ready(function() {
 	//init code
@@ -49,55 +83,63 @@ $(document).ready(function() {
 			//display first question & answers
 			$('#question').html(question);
 
-			for (var i = 0; i < answers.length; i++) {
-				$('#answers').append('<label><input type="radio" name="answer" value="' + answers[i] + '">' + answers[i] + '</label><br />');
-			}
+			getAnswers();
 
 			registerAnswers();
 
 		});
 
-
-
-
 		//"next" button - manage the questions dynamically
 		$('#nextQuestion').click(function() {
 
-			//hide next button
-			$(this).css('visibility', 'hidden');
-
-			//get value of selected answer
-			var answerSel = $('input:checked').val();
-			console.log(answerSel);
-
-			//increment the question number
-			currentQ = currentQ + 1 ;
-
-			question = sortQuestObj[currentQ].question;
-			answers = sortQuestObj[currentQ].choices;
-			totalQ = sortQuestObj.length;
-			lastQ = sortQuestObj.length - 1;
-
-			//grab the last element of array
-			if (currentQ === lastQ) {
-				//change text of button to "Total Score"
-				 $('#nextQuestion').text("Total Score");
-			}
+			//get value text of bottom
+			 var totalScore = $(this).prop("value");
 
 
-			if (!answerSel) {
-				$('#errorMsg').text('You must select one of the answers');
-			}else {
-				$('#answers').empty();
-				$('#errorMsg').hide();
-				$('#question').html(question);
-				for (var i = 0; i < answers.length; i++) {
-				$('#answers').append('<label><input type="radio" class="radioBtn" name="answer" value="' + answers[i] + '">' + answers[i] + '</label><br />');
+			 	if (totalScore === 'Total Score') {
+			 		// print total score
+					$('#finalSentence').html('you got '+ score + ' correct answers, out of ' + totalQ );
+				}else {
+					//dynamic to get the next question
+
+					//hide next button
+					$(this).css('visibility', 'hidden');
+
+					//get value of selected answer
+					var answerSel = $('input:checked').val();
+
+					//increment the question number
+					currentQ = currentQ + 1 ;
+
+					question = sortQuestObj[currentQ].question;
+					answers = sortQuestObj[currentQ].choices;
+					totalQ = sortQuestObj.length;
+					lastQ = sortQuestObj.length - 1;
+
+					//grab the last element of array
+					if (currentQ === lastQ) {
+						//change text of button to "Total Score"
+						 $('#nextQuestion').text("Total Score");
+						 $('#nextQuestion').attr('value', 'Total Score' );
+						 console.log(score);
+						 console.log(scoreWrong);
+					}
+
+					$('#answers').empty();
+					$('#errorMsg').hide();
+					$('#question').html(question);
+
+
+					// if (!answerSel) {
+					// 	$('#errorMsg').text('You must select one of the answers');
+					// }else {}
+
+					getAnswers();
+
+					registerAnswers();
+
 				}
 
-				registerAnswers();
-			}
 		});
-
 
 });
